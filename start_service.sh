ae77b5b6096b3f0f862ec0cd8c210cb53f500c24
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2023-08-25 12:50:48 +0200

    Add service for webserver

--- start_service.sh
+++ start_service.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#TODO: change $HOME in the paths
-
-SERVICE_NAME="pingdashboard"
-EXECUTABLE_PATH="$HOME/pingdashboard/ping_contestant_machines.py"
+SERVICE_NAME="pingdashboard-pinger"
+EXECUTABLE_PATH="$HOME/ioi/pingdashboard/ping_contestant_machines.py"
 
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 sudo tee $SERVICE_FILE > /dev/null <<EOF
@@ -12,7 +10,7 @@ Description=Ping Contestant Machines
 
 [Service]
 ExecStart=/usr/bin/python3 $EXECUTABLE_PATH
-WorkingDirectory=$HOME/pingdashboard
+WorkingDirectory=$HOME/ioi/pingdashboard
 Restart=always
 User=$USER
 
@@ -25,4 +23,28 @@ sudo systemctl daemon-reload
 sudo systemctl enable $SERVICE_NAME
 sudo systemctl start $SERVICE_NAME
 
-sudo systemctl status $SERVICE_NAME
+echo "Started $SERVICE_NAME, see status with command 'systemctl status $SERVICE_NAME'"
+
+SERVICE_NAME="pingdashboard-webserver"
+
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+sudo tee $SERVICE_FILE > /dev/null <<EOF
+[Unit]
+Description=Pingdashboard Webserver
+
+[Service]
+ExecStart=/usr/bin/python3 -m http.server 8080
+WorkingDirectory=$HOME/ioi/pingdashboard/public
+Restart=always
+User=$USER
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl start $SERVICE_NAME
+
+echo "Started $SERVICE_NAME, see status with command 'systemctl status $SERVICE_NAME'"
